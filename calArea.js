684e30f32357a7da31857c5766911dddfbabaf65
ANUJ KUMAR YADAV <anujf0510@gmail.com>
2021-08-21 20:36:14 +0530

    add footer

--- calArea.js
+++ calArea.js
@@ -1,14 +1,14 @@
-const base = document.querySelector('#base');
-const height = document.querySelector('#height');
-const calAreaBtn = document.querySelector('#cal-area-btn')
-const output = document.querySelector('#output')
-function calculateArea(){
+const base = document.querySelector("#base");
+const height = document.querySelector("#height");
+const calAreaBtn = document.querySelector("#cal-area-btn");
+const output = document.querySelector("#output");
+
+function calculateArea() {
     let baseValue = Number(base.value);
     let heightValue = Number(height.value);
-    let area = baseValue * heightValue / 2;
-    console.log(area)
-    output.innerText = `The area of the triangle is ${area} cm²`
+    let area = (baseValue * heightValue) / 2;
+    console.log(area);
+    output.innerText = `The area of the triangle is ${area} cm²`;
 }
 
-
-calAreaBtn.addEventListener('click',calculateArea)
+calAreaBtn.addEventListener("click", calculateArea);
--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const triangleAngle = document.querySelectorAll('.triangles-angle');
-const isTriangleBtn = document.querySelector('#is-triangle-btn');
-const output = document.querySelector('#output');
+const triangleAngle = document.querySelectorAll(".triangles-angle");
+const isTriangleBtn = document.querySelector("#is-triangle-btn");
+const output = document.querySelector("#output");
 
-function calculateSumOfAngles(angle1,angle2,angle3){
-    const sumOfAngles = angle1 + angle2 + angle3;
-   return sumOfAngles
+function calculateSumOfAngles(angle1, angle2, angle3) {
+  const sumOfAngles = angle1 + angle2 + angle3;
+  return sumOfAngles;
 }
 
-function isTriangle(){
-    const sumOfAngles = calculateSumOfAngles(Number(triangleAngle[0].value),Number(triangleAngle[1].value),Number(triangleAngle[2].value))
-    console.log(sumOfAngles);
-    if(sumOfAngles === 180){
-        output.innerText = 'Yayy, the angles form a triangle'
-    }else{
-        output.innerText = 'Ohh!, the angles do not form a triangle'
-    }
+function isTriangle() {
+  const sumOfAngles = calculateSumOfAngles(
+    Number(triangleAngle[0].value),
+    Number(triangleAngle[1].value),
+    Number(triangleAngle[2].value)
+  );
+  console.log(sumOfAngles);
+  if (sumOfAngles === 180) {
+    output.innerText = "Yayy, the angles form a triangle";
+  } else {
+    output.innerText = "Ohh!, the angles do not form a triangle";
+  }
 }
 
-isTriangleBtn.addEventListener('click', isTriangle)
+isTriangleBtn.addEventListener("click", isTriangle);
